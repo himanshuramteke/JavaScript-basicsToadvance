be6bbba68960a776802c2548fc10bc0331bda42b
himanshuramteke <himanshuramteke007@gmail.com>
2023-07-18 23:13:25 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 35478;
+let accountEmail = "himanshu@google.com";
+var accountPass = "12344";
+accountCity = "Nagpur";
+let accountState ;
+
+// accountId = 23; // not allowed because we can define const only one time;
+
+accountEmail = "himanshu@microsoft.com"
+accountPass = "123212"
+accountCity = "Bangalore"
+
+/*
+Prefer not to use var
+because of issue in block and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPass,accountState,accountCity])
+
+
+
+
+
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all JS code as a newer version
+
+// alert (3 + 3); We are using Node.js, not browser
+
+
+
+
+let name = "Himanshu"
+let age = 23
+let isloggedIn = true
+
+/* PRIMITIVE  DATA TYPES */
+
+//string => ""
+//number => 2 to power 53
+//bigint 
+//boolean => true/false
+//null => Standalone Value
+//undefined =>
+//symbol => unique
+
+/* NON-PRIMITIVE */
+// Object
+// Array
+//Function
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //Object
