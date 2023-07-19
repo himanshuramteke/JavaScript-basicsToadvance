1591a75b3aa1db0b33da58e3c2b9abc90c5fef33
himanshuramteke <himanshuramteke007@gmail.com>
2023-07-19 11:26:00 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,27 @@
+let score = "Himanshu"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInnumber = Number(score);
+console.log(typeof valueInnumber);
+console.log(valueInnumber);
+
+//"33" => 33
+//"33abc" => NaN {Not a Number};
+//true =>1; false =>0; 
+
+let isloggedIn = "himanshu";
+
+let BooleanIsLoggedIn = Boolean(isloggedIn);
+console.log(BooleanIsLoggedIn);
+
+//1 => true; 0 => false;
+//"" => false
+//"himanshu" => true
+
+let someNumber = 34
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
