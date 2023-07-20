03155e49c8834dfbb28426f1da53c076f577e7f4
himanshuramteke <himanshuramteke007@gmail.com>
2023-07-20 23:25:42 +0530

    arrays part1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,38 @@
+//array
+
+const myArr = [0,1,2,3,4]
+const myHeroes = ["Ironman","Thor"]
+
+const myArr2 = new Array(1,2,3,4)
+//console.log(myArr[1]);
+
+//Arrays methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(2)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(4));
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+//slice, splice
+
+console.log("A ",myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C ",myArr);
+console.log(myn2);
+
+
