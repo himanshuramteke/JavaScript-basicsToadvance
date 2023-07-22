22713db63bd85487328401ea20aebd6312c5e1be
himanshuramteke <himanshuramteke007@gmail.com>
2023-07-22 18:09:35 +0530

    functions part1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,35 @@
+
+function sayMyname(){
+    console.log("H");
+    console.log("I");
+    console.log("M");
+    console.log("A");
+    console.log("N");
+    console.log("S");
+    console.log("H");
+    console.log("U");
+}
+//sayMyname();
+
+function addTwoNumbers (number1, number2){
+    console.log(number1 + number2);
+}
+addTwoNumbers(3, 4)
+
+function addTwoNumbers (number1, number2){
+    let result = number1 + number2
+    return result;
+}
+const result = addTwoNumbers(3, 4)
+
+//console.log("result : ", result);
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("please enter a username");
+        return
+    }
+   return `${username} just logged in`
+}
+
+console.log(loginUserMessage("myself"))
