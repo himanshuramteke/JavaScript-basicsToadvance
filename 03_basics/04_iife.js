5665638060fea06ad3700e5f4fa0701cf40852ff
himanshuramteke <himanshuramteke007@gmail.com>
2023-07-23 23:18:06 +0530

    iife talks

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+// Immediately Invoked Function Expression
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
