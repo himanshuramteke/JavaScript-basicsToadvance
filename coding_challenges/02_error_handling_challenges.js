c084d21b9df1907961ef193b6247be85251135f8
himanshuramteke <himanshuramteke007@gmail.com>
2023-08-06 22:10:41 +0530

    completed error handling assignment

--- coding_challenges/02_error_handling_challenges.js
+++ coding_challenges/02_error_handling_challenges.js
@@ -122,6 +122,163 @@ class Car {
   const person2 = new Person();
   console.log(person2.getDetails()); // Output: "Name: Unknown, Age: 0"
   
-  
-  
+/*
+
+6. Using Static Method to Add Two Numbers with Calculator Class
+
+Create a class called Calculator with a static method called add. The add method should take two numbers as arguments and return their sum. Instantiate the Calculator class and call the add method.
+
+Expected Output:
+
+console.log(result); // Output: 15
+
+*/
+
+class Calculator {
+  static add(num1, num2) {
+    return num1 + num2;
+  }
+}
+
+const result = Calculator.add(5, 10);
+
+// Expected Output:
+console.log(result); // Output: 15
+
+/*
+
+7. Password Checker.
+
+Create a class called User with properties username and password. Implement a getter method for password that returns the password with all characters replaced by asterisks. Implement a setter method for password that checks if the new password is at least 8 characters long and contains at least one number and one uppercase letter. If the password is valid, set the new password. If not, log an error message.
+
+Expected output:
+const user = new User("johndoe", "Password123");
+console.log(user.getPassword()); // ***********
+
+user.setPassword("myPassword"); // Error: Password must be at least 8 characters long and contain at least one number and one uppercase letter.
+
+user.setPassword("MyPassword"); // Error: Password must be at least 8 characters long and contain at least one number and one uppercase letter.
+
+user.setPassword("Mypassword123");
+console.log(user.getPassword()); // *************
+
+*/
+
+class User {
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+  }
+
+  getPassword() {
+    return this.password.replace(/./g, "*");
+  }
+
+  setPassword(newPassword) {
+    let containsNumber = false;
+    let containsUppercase = false;
+    for (let i = 0; i < newPassword.length; i++) {
+      const char = newPassword.charAt(i);
+      if (!isNaN(char)) {
+        containsNumber = true;
+      } else if (char === char.toUpperCase()) {
+        containsUppercase = true;
+      }
+    }
+    if (newPassword.length >= 8 && containsNumber && containsUppercase) {
+      this.password = newPassword;
+    } else {
+      console.log(
+        "Error: Password must be at least 8 characters long and contain at least one number and one uppercase letter."
+      );
+    }
+  }
+}
+
+// Expected output:
+const user = new User("johndoe", "Password123");
+console.log(user.getPassword()); // ***********
+
+user.setPassword("myPassword"); // Error: Password must be at least 8 characters long and contain at least one number and one uppercase letter.
+user.setPassword("MyPassword"); // Error: Password must be at least 8 characters long and contain at least one number and one uppercase letter.
+user.setPassword("Mypassword123");
+console.log(user.getPassword()); // *************
+
+
+// 8. Adding a Method to a Prototype.
+
+// Create a prototype object called Student with a property name. Add a method called printDetails to the prototype that logs the string "Hello, my name is {name}" to the console. Instantiate a Student object with the name "Mithun" and call the printDetails method.
+
+// Expected Output
+
+// const student = new Student("Mithun");
+// student.printDetails(); // "Hello, the student is Mithun"
+
+function Student(name) {
+  this.name = name;
+}
+
+Student.prototype.printDetails = function () {
+  console.log(`Hello, my name is ${this.name}`);
+};
+
+// Expected Output
+
+const student = new Student("Mithun");
+student.printDetails(); // "Hello, the student is Mithun"
+
+/*
+
+9. Check the presence using closures.
+
+Create a numberChecker function that takes an array of numbers as an argument and returns a function. The returned function should take another number as an argument and return true if the number is in the array, and false otherwise.
+
+
+*/
+
+function numberChecker(numbers) {
+  return function (num) {
+    return numbers.includes(num);
+  };
+}
+
+// Expected Result:
+const arr = [1, 2, 3, 4, 5];
+const checkNum = numberChecker(arr);
+
+console.log(checkNum(3)); // true
+console.log(checkNum(6)); // false
+
+/*
+
+10. Filter by Category.
+
+Write a function that takes an array of products and returns a function that filters the array by a given product category. The function must filter an eCommerce products array by a specific category. The closure filters products using the filter() method. Finally, it returns a new array containing only the products with the same category as the input.
+
+
+*/
+
+function filterByCategory(products) {
+  return function (category) {
+    return products.filter(function (product) {
+      return product.category === category;
+    });
+  };
+}
+
+// Expected Output
+
+var products = [
+  { name: "Shirt", category: "Clothing" },
+  { name: "Pants", category: "Clothing" },
+  { name: "Hat", category: "Accessories" },
+  { name: "Sunglasses", category: "Accessories" },
+];
+
+var clothingProducts = filterByCategory(products)("Clothing");
+
+console.log(clothingProducts);
+// Output: [{name: "Shirt", category: "Clothing"}, {name: "Pants", category: "Clothing"}]
+
+
   
